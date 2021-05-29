b0201b7f240cb799851af8d484c72de7fe3cd05a
Ghenghiz <onki.herb@googlemail.com>
2021-05-29 21:28:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hallo Herb!'));
+app.get('/', (req, res) => res.send('Hallo WERAUCHIMMER!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
